099f2c3cc0454c6cf11724cb5fecb2a2b7ba8759
BRonen <brennopereira6@gmail.com>
2025-11-27 13:42:45 -0300

    feat: add clojure example of callbacks

--- dev.clj
+++ dev.clj
@@ -1,12 +1,20 @@
 (ns dev
-  (:import [Accumulator Acc]))
+  (:import [Accumulator Acc]
+           [Callbacks Callbacks]))
 
 (def acc (Acc.))
 
 (comment
-
   (.accumulate acc 1)
-
   (.getValue acc)
+
+  nil)
+
+(def callbacks (Callbacks.))
+
+(comment
+  (.apply_callback callbacks 2 (fn [x] (* x 5)))
+
+  (.apply_bicallback callbacks 3 2 (fn [x y] (* x y)))
 
   nil)
